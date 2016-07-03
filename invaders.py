de4ecee37dbf8f8fab47bc819fd3fdaf563a1f58
HSSCZ <HSSCZ@users.noreply.github.com>
2016-07-03 02:22:07 -0600

    Color modes & lots of misc shit

--- invaders.py
+++ invaders.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+import argparse
 import os
 import random
 import shutil
@@ -16,32 +18,38 @@ TERM_WIDTH, TERM_HEIGHT = shutil.get_terminal_size()
 RESET = '\x1b[0m'
 
 def print_invaders(rows, num_invaders, mode=None):
-    for i in range(rows):
-        print_row(num_invaders, mode=mode)
+    if mode == 'sys':
+        print(system_colors(), end='')
+    else:
+        topspace = (TERM_HEIGHT - ((INVADER_HEIGHT + 2) * rows)) // 2 - 1
+        print('\n'*topspace)
+        for i in range(rows):
+            print_row(num_invaders, mode=mode)
 
 def print_row(num_invaders, mode=None):
-    ws = (TERM_WIDTH - (INVADER_WIDTH * num_invaders)) // 2
-
-    if mode == None or mode == 'random_row':
-        print(random_row(ws, num_invaders))
-    elif mode == 'random_all':
-        print(random_all(ws, num_invaders))
-    elif mode == 'random_line':
-        print(random_line(ws, num_invaders))
+    if mode == 'row':
+        print(random_row(num_invaders), end='')
+    elif mode == 'all':
+        print(random_all(num_invaders), end='')
+    elif mode == 'line':
+        print(random_line(num_invaders), end='')
     else:
-        print(random_row(ws, num_invaders))
+        print(random_row(num_invaders), end='')
 
-def random_row(whitespace, num_invaders):
+def random_row(num_invaders):
+    whitespace = (TERM_WIDTH - (INVADER_WIDTH * num_invaders)) // 2
     color = '\x1b[38;5;%dm' % random.randint(1, 255)
     string = '\n'
     for i in range(INVADER_HEIGHT):
         string += ' '*whitespace
         string += color + INVADER[i]*num_invaders
         string += '\n'
+    string += '\n'
     string += RESET
     return string
 
-def random_all(whitespace, num_invaders):
+def random_all(num_invaders):
+    whitespace = (TERM_WIDTH - (INVADER_WIDTH * num_invaders)) // 2
     colors = [random.randint(1, 255) for _ in range(num_invaders)]
     string = '\n'
     for i in range(INVADER_HEIGHT):
@@ -49,10 +57,12 @@ def random_all(whitespace, num_invaders):
         for j in range(num_invaders):
             string += '\x1b[38;5;%dm'%colors[j] + INVADER[i]
         string += '\n'
+    string += '\n'
     string += RESET
     return string
 
-def random_line(whitespace, num_invaders):
+def random_line(num_invaders):
+    whitespace = (TERM_WIDTH - (INVADER_WIDTH * num_invaders)) // 2
     string = '\n'
     for i in range(INVADER_HEIGHT):
         string += ' '*whitespace
@@ -60,21 +70,72 @@ def random_line(whitespace, num_invaders):
             color = random.randint(1, 255)
             string += '\x1b[38;5;%dm'%color + INVADER[i]
         string += '\n'
+    string += '\n'
     string += RESET
     return string
 
-def main():
-    if TERM_WIDTH <= INVADER_WIDTH or TERM_HEIGHT <= INVADER_HEIGHT:
+def system_colors():
+    if TERM_WIDTH <= INVADER_WIDTH * 6:
+        exit('Current terminal too small: Increase width')
+    if TERM_HEIGHT <= (INVADER_HEIGHT * 3) + 6:
+        exit('Current terminal too small: Increase height')
+
+    whitespace = (TERM_WIDTH - (INVADER_WIDTH * 6)) // 2
+    topspace = (TERM_HEIGHT - ((INVADER_HEIGHT + 2) * 3)) // 2
+    colors = [_ for _ in range(1,7)] + [_ for _ in range(9,15)]
+
+    #grayscale
+    gs_whitespace= (TERM_WIDTH - (INVADER_WIDTH * 4)) // 2
+    gs_colors = [0, 8, 7, 15]
+
+    string = '\n' * topspace
+    for i in range(2):
+        string += '\n'
+        for j in range(INVADER_HEIGHT):
+            string += ' '*whitespace
+            for k in range(6):
+                if i == 1:
+                    string += '\x1b[38;5;%dm'%colors[6+k]+ INVADER[j]
+                else:
+                    string += '\x1b[38;5;%dm'%colors[k]+ INVADER[j]
+            string += '\n'
+        string += '\n'
+    # grayscale colors
+    string += '\n'
+    for i in range(INVADER_HEIGHT):
+        string += ' '*gs_whitespace
+        for j in range(4):
+            string += '\x1b[38;5;%dm'%gs_colors[j] + INVADER[i]
+        string += '\n'
+    string += RESET
+    return string
+
+def main(args):
+    # INVADER_HEIGHT + 2 because each row gets a newline before and after
+    if TERM_WIDTH <= INVADER_WIDTH or TERM_HEIGHT <= INVADER_HEIGHT + 2:
         exit('Current terminal too small')
 
+    modes = ['row', 'line', 'all', 'sys']
     num_invaders = TERM_WIDTH // INVADER_WIDTH
     num_rows     = TERM_HEIGHT // (INVADER_HEIGHT + 2)
 
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print_invaders(num_rows, num_invaders)
+    if args.c and args.c in modes:
+        print_invaders(num_rows, num_invaders, mode=args.c)
+    else:
+        print_invaders(num_rows, num_invaders, mode='row')
+    if args.i:
+        input()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(prog='invaders')
+    parser.add_argument('-i', action='store_true', help='Wait for input before quitting')
+    parser.add_argument('-c', default='row', help='Color mode [row, line, all, sys]')
+    args = parser.parse_args()
+
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        exit()
 
 '''
 
