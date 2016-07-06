c5c51f3d15eadacb44729f9b820cdcb4fd578549
HSSCZ <HSSCZ@users.noreply.github.com>
2016-07-06 00:56:27 -0600

    clear term arg, docstrings, some variables and args adjusted

--- invaders.py
+++ invaders.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+'''
+Fills the terminal with invaders!
+
+usage: invaders [-h] [-c {row,seg,all,sys}] [-i] [-l]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c {row,seg,all,sys}  Color mode (default: row)
+  -i                    Wait for Enter keypress before quitting
+  -l, -L                Clear terminal before printing invaders
+'''
 import argparse
 import os
 import random
@@ -17,27 +28,44 @@ TERM_WIDTH, TERM_HEIGHT = shutil.get_terminal_size()
 
 RESET = '\x1b[0m'
 
-def print_invaders(rows, num_invaders, mode=None):
-    if mode == 'sys':
-        print(system_colors(), end='')
-    else:
-        topspace = (TERM_HEIGHT - ((INVADER_HEIGHT + 2) * rows)) // 2 - 1
-        print('\n'*topspace)
-        for i in range(rows):
-            print_row(num_invaders, mode=mode)
+def print_invaders(num_rows, num_invaders, mode, clear):
+    '''
+    Prints a grid of invaders `num_invaders` wide by `num_rows` tall
 
-def print_row(num_invaders, mode=None):
-    if mode == 'row':
-        print(random_row(num_invaders), end='')
-    elif mode == 'all':
-        print(random_all(num_invaders), end='')
-    elif mode == 'line':
-        print(random_line(num_invaders), end='')
-    else:
-        print(random_row(num_invaders), end='')
-
-def random_row(num_invaders):
+    Colored according to `mode` which must be one of 'row', 'seg', 'all', 'sys'
+    Clear the terminal before printing if `clear` is True
+    Terminal is always cleared if mode is 'sys'
+    '''
+    # leading whitespace for invader strings
     whitespace = (TERM_WIDTH - (INVADER_WIDTH * num_invaders)) // 2
+    topspace = (TERM_HEIGHT - ((INVADER_HEIGHT + 2) * num_rows)) // 2 - 1
+
+    if clear:
+        os.system('clear')
+
+    print('\n' * topspace)
+    if mode == 'sys':
+        if not clear:
+            os.system('clear')
+            print('\n' * topspace)
+        print(system_colors(whitespace), end='')
+    else:
+        for i in range(num_rows):
+            print_row(num_invaders, whitespace, mode)
+
+def print_row(num_invaders, whitespace, mode):
+    ''' Prints a row of invaders '''
+    if mode == 'row':
+        print(random_row(num_invaders, whitespace), end='')
+    elif mode == 'all':
+        print(random_all(num_invaders, whitespace), end='')
+    elif mode == 'seg':
+        print(random_seg(num_invaders, whitespace), end='')
+    else:
+        print(random_row(num_invaders, whitespace), end='')
+
+def random_row(num_invaders, whitespace):
+    ''' Prints a row of invaders with the entire row a random color '''
     color = '\x1b[38;5;%dm' % random.randint(1, 255)
     string = '\n'
     for i in range(INVADER_HEIGHT):
@@ -48,8 +76,8 @@ def random_row(num_invaders):
     string += RESET
     return string
 
-def random_all(num_invaders):
-    whitespace = (TERM_WIDTH - (INVADER_WIDTH * num_invaders)) // 2
+def random_all(num_invaders, whitespace):
+    ''' Prints a row of invaders with each invader a random color '''
     colors = [random.randint(1, 255) for _ in range(num_invaders)]
     string = '\n'
     for i in range(INVADER_HEIGHT):
@@ -61,8 +89,8 @@ def random_all(num_invaders):
     string += RESET
     return string
 
-def random_line(num_invaders):
-    whitespace = (TERM_WIDTH - (INVADER_WIDTH * num_invaders)) // 2
+def random_seg(num_invaders, whitespace):
+    ''' Prints a row of invaders with each segment a random color '''
     string = '\n'
     for i in range(INVADER_HEIGHT):
         string += ' '*whitespace
@@ -74,21 +102,19 @@ def random_line(num_invaders):
     string += RESET
     return string
 
-def system_colors():
+def system_colors(whitespace):
+    ''' Prints a system color display of 6, 6, 4 invaders centered on screen '''
     if TERM_WIDTH <= INVADER_WIDTH * 6:
         exit('Current terminal too small: Increase width')
     if TERM_HEIGHT <= (INVADER_HEIGHT * 3) + 6:
         exit('Current terminal too small: Increase height')
-
-    whitespace = (TERM_WIDTH - (INVADER_WIDTH * 6)) // 2
-    topspace = (TERM_HEIGHT - ((INVADER_HEIGHT + 2) * 3)) // 2
-    colors = [_ for _ in range(1,7)] + [_ for _ in range(9,15)]
+    colors = list(range(1,7)) + list(range(9,15))
 
     #grayscale
     gs_whitespace= (TERM_WIDTH - (INVADER_WIDTH * 4)) // 2
     gs_colors = [0, 8, 7, 15]
 
-    string = '\n' * topspace
+    string = ''
     for i in range(2):
         string += '\n'
         for j in range(INVADER_HEIGHT):
@@ -111,21 +137,28 @@ def system_colors():
     return string
 
 def main(args):
-    # INVADER_HEIGHT + 2 because each row gets a newline before and after
-    if TERM_WIDTH <= INVADER_WIDTH or TERM_HEIGHT <= INVADER_HEIGHT + 2:
+    if TERM_WIDTH < INVADER_WIDTH or TERM_HEIGHT < INVADER_HEIGHT:
         exit('Current terminal too small')
 
-    num_invaders = TERM_WIDTH // INVADER_WIDTH
-    num_rows     = TERM_HEIGHT // (INVADER_HEIGHT + 2)
+    mode = args.c
+    clear = args.l
 
-    print_invaders(num_rows, num_invaders, mode=args.c)
+    num_invaders = 6 if mode == 'sys' else TERM_WIDTH // INVADER_WIDTH
+    num_rows     = 3 if mode == 'sys' else TERM_HEIGHT // (INVADER_HEIGHT + 2)
+
+    print_invaders(num_rows, num_invaders, mode, clear)
+
     if args.i:
         input()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='invaders')
-    parser.add_argument('-i', action='store_true', help='Wait for input before quitting')
-    parser.add_argument('-c', default='row', help='Color mode [row, line, all, sys]')
+    parser.add_argument('-c', default='row', choices=['row', 'seg', 'all', 'sys'],
+                        help='Color mode (default: %(default)s)')
+    parser.add_argument('-i', action='store_true',
+                        help='Wait for Enter keypress before quitting')
+    parser.add_argument('-l', '-L', action='store_true',
+                        help='Clear terminal before printing invaders')
     args = parser.parse_args()
 
     try:
